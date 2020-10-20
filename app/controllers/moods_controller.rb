98f907a293eec3be7ba8dabd3a6652589df7972f
aribakhan97 <akhan41@binghamton.edu>
2020-10-19 21:42:40 -0400

    mood tracker post api

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -4,4 +4,19 @@ class MoodsController < ApplicationController
         moods = Mood.all
         render json: moods
     end
+
+    def new 
+        @mood = Mood.new
+    end 
+
+    def create 
+        @mood = Mood.create(mood_params)  
+    end 
+
+    private
+
+    def mood_params 
+        params.require(:mood).permit!
+    end 
+    
 end
