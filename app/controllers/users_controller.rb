6a9550e5be284541c1881000578ee498eb26e011
jakeFromm <jacobfromm@gmail.com>
2020-10-22 18:53:30 -0400

    merging from jakeF

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+    
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    private
+
+    def user_params 
+        params.require(:user).permit!
+    end 
 end
